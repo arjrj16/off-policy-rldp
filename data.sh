167cc19f772333e1260c528de9ae10c4ed1d608b
Arjun Melwani <arjunmelwani@gmail.com>
2025-12-03 18:00:41 -0500

    data get script

---
+++ data.sh
@@ -0,0 +1,28 @@
+# 1. Set your data directory (adjust path as needed)
+export DPPO_DATA_DIR=/home/melwani/67920/paper-gits/dppo/data  # or wherever your data is
+
+cd /home/melwani/67920
+# 2. Create a temp directory for the download
+mkdir -p tmp/robomimic_download
+
+# 3. Download the transport HDF5 (proficient-human, low-dim)
+cd tmp/robomimic_download
+# wget https://diffusion-policy-data.s3.us-west-2.amazonaws.com/data/robomimic/datasets/transport/ph/low_dim.hdf5
+
+
+
+# Transport PH low-dim (direct link from robomimic v0.1)
+wget http://downloads.cs.stanford.edu/downloads/rt_benchmark/transport/ph/low_dim.hdf5
+
+    
+# 4. Go back to your project and run the processing script
+cd /home/melwani/67920/paper-gits/dppo  # your project directory
+
+# Then process
+uv run python script/dataset/process_robomimic_dataset.py \
+    --load_path /home/melwani/67920/tmp/robomimic_download/low_dim.hdf5 \
+    --save_dir $DPPO_DATA_DIR/robomimic/transport/ \
+    --normalize
+
+# 5. Clean up
+# rm -rf /tmp/robomimic_download
